0958092cbb73b109a5d1cc2bb45521d22e235a1f
Jose Juan Escalante <jjet@live.com.mx>
2019-01-05 11:14:37 -0600

    [REF] openacademy: Add translation

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api, exceptions, _
 import time
 from psycopg2 import IntegrityError
 from datetime import timedelta
@@ -31,11 +31,11 @@ class Course(models.Model):
     def copy(self, default=None):
         if default is None:
             default = {}
-        copied_count = self.search_count([('name', 'ilike', 'Copy of %s%%' % (self.name))])
+        copied_count = self.search_count([('name', 'ilike', _('Copy of %s%%') % (self.name))])
         if not copied_count:
-            new_name = "Copy of %s" % (self.name)
+            new_name =_("Copy of %s") % (self.name)
         else:
-            new_name = "Copy of %s (%s)"% (self.name, copied_count)
+            new_name = _("Copy of %s (%s)")% (self.name, copied_count)
         default['name'] = new_name
         return super(Course, self).copy(default)
 
@@ -96,16 +96,16 @@ class Session(models.Model):
             self.active = False
             return {
                     'warning':{
-                        'title':"Incorrect 'seats' value",
-                        'message': "The number of available seats may not be negative",
+                        'title':_("Incorrect 'seats' value"),
+                        'message': _("The number of available seats may not be negative"),
                         }
                     }
         if self.seats < len(self.attendee_ids):
             self.active = False
             return {
                     'warning': {
-                        'title':"Too many attendees",
-                        'message':"Increase seats or remove excess attendees",
+                        'title':_("Too many attendees"),
+                        'message':_("Increase seats or remove excess attendees"),
                         }
                     }
         self.active = True
@@ -114,4 +114,4 @@ class Session(models.Model):
     def _check_instructor_not_in_attendees(self):
         for record in self.filtered('instructor_id'):
             if record.instructor_id in record.attendee_ids:
-                raise exceptions.ValidationError("A session's instructor can't be an attendee")
+                raise exceptions.ValidationError(_("A session's instructor can't be an attendee"))
