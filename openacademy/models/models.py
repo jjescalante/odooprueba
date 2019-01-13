53fb94bf230d7821e2edb6391dea5e29406d4cd1
Jose Juan Escalante <jjet@live.com.mx>
2019-01-13 11:58:17 -0600

    [REF] travis: Correction of errors shown by travis 2

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -3,40 +3,40 @@
 from odoo import models, fields, api, exceptions, _
 from datetime import timedelta
 
-    def get_uid(self, *a):
-        return self.env.uid
+def get_uid(self, *a):
+    return self.env.uid
 
-    class Course(models.Model):
-        _name = 'openacademy.course'
+class Course(models.Model):
+    _name = 'openacademy.course'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', string="Responsible",
-                                    index=True, ondelete='set null',
-                                    default=get_uid)
+        index=True, ondelete='set null',
+        default=get_uid)
     session_ids = fields.One2many('openacademy.session', 'course_id')
 
     _sql_constraints = [
-            ('name_description_check',
-            'CHECK( name != description )',
-            "The title of course should not be the description"),
-            ('name_unique', 'UNIQUE(name)',
-            "The course title must be unique",),
-            ]
+        ('name_description_check',
+        'CHECK( name != description )',
+        "The title of course should not be the description"),
+        ('name_unique', 'UNIQUE(name)',
+        "The course title must be unique",),
+        ]
 
-    def copy(self, default=None):
+def copy(self, default=None):
         if default is None:
             default = {}
-        copied_count = self.search_count([('name', 'ilike',
-            _('Copy of %s%%') % (self.name))])
+        copied_count = self.search_count([('name',
+            'ilike', _('Copy of %s%%') % (self.name))])
         if not copied_count:
-            new_name =_("Copy of %s") % (self.name)
+            new_name = _("Copy of %s") % (self.name)
         else:
-            new_name = _("Copy of %s (%s)")% (self.name, copied_count)
+            new_name = _("Copy of %s (%s)") % (self.name, copied_count)
         default['name'] = new_name
         return super(Course, self).copy(default)
 
-    class Session(models.Model):
+class Session(models.Model):
         _name = 'openacademy.session'
 
     name = fields.Char(required=True)
@@ -44,24 +44,19 @@ from datetime import timedelta
     datetime_test = fields.Datetime(default=fields.Datetime.now)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string='Instructor', domain=['|',('instructor', '=', 'True'), ('category_id.name','ilike', 'Teacher')])
-    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    instructor_id = fields.Many2one('res.partner', string='Instructor',
+        domain=['|', ('instructor', '=', 'True'),
+        ('category_id.name', 'ilike', 'Teacher')])
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(compute='_taken_seats', store=True)
     active = fields.Boolean(default=True)
-    end_date = fields.Date(store=True, compute='_get_end_date', inverse='_set_end_date')
-    attendees_count = fields.Integer(compute='_get_attendees_count', store=True)
+    end_date = fields.Date(store=True,
+        compute='_get_end_date', inverse='_set_end_date')
+    attendees_count = fields.Integer(compute='_get_attendees_count',
+        store=True)
     color = fields.Float()
-    #hours = fields.Float(string="Duration in hours", compute='_get_hours', inverse='_set_hours')
-
-    #@api.depends('duration')
-    #def _get_hours(self):
-    #    for r in self:
-    #        r.hours = r.duration * 24
-
-    #def _set_hours(self):
-    #    for r in self:
-    #        r.duration = r.hours / 24
 
     @api.depends('attendee_ids')
     def _get_attendees_count(self):
@@ -72,8 +67,8 @@ from datetime import timedelta
     def _get_end_date(self):
         for record in self.filtered('start_date'):
             start_date = fields.Datetime.from_string(record.start_date)
-            record.end_date = start_date + timedelta(days=record.duration, seconds=-1)
-
+            record.end_date = start_date + timedelta(days=record.duration,
+                seconds=-1)
 
     @api.depends('start_date', 'duration')
     def _set_end_date(self):
@@ -85,7 +80,8 @@ from datetime import timedelta
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         for record in self.filtered(lambda r: r.seats):
-            record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
+            record.taken_seats = 100.0 * len(record.attendee_ids)
+            / record.seats
 
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -7,8 +7,8 @@ from odoo import models, fields
 
     # Add a new column to the res.partner model, by default partners are not
     # instructors
-    instructor = fields.Boolean(default=False)
-    session_ids = fields.Many2many('openacademy.session',
-                  string="Attended Sessions", readonly=True)
-    other_field = fields.Boolean(default=True)
-    other_field2 = fields.Boolean(default=True)
+        instructor = fields.Boolean(default=False)
+        session_ids = fields.Many2many('openacademy.session',
+                    string="Attended Sessions", readonly=True)
+        other_field = fields.Boolean(default=True)
+        other_field2 = fields.Boolean(default=True)
