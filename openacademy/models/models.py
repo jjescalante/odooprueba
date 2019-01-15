449d8e59f8dac97a9ac4fffcce7dffd7c89dd59f
Jose Juan Escalante <jjet@live.com.mx>
2019-01-14 20:30:42 -0600

    [REF] test flake8: 4

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields, api, exceptions, _
 from datetime import timedelta
 
@@ -12,26 +13,25 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', string="Responsible",
-                    index=True, ondelete='set null', default=get_uid)
+            index=True, ondelete='set null', default=get_uid)
     session_ids = fields.One2many('openacademy.session', 'course_id')
     _sql_constraints = [('name_description_check',
-                        'CHECK( name != description )',
-                        "The title of course should not be the description"),
-                        ('name_unique', 'UNIQUE(name)',
-                        "The course title must be unique",),]
+            'CHECK( name != description )',
+            "The title of course should not be the description"),
+            ('name_unique', 'UNIQUE(name)', "The course title must be unique", ), ]
 
 
 def copy(self, default=None):
-        if default is None:
-        default = {}
-        copied_count = self.search_count([('name',
-            'ilike', _('Copy of %s%%') % (self.name))])
-        if not copied_count:
+    if default is None:
+            default = {}
+    copied_count = self.search_count([('name', 'ilike',
+            _('Copy of %s%%') % (self.name))])
+    if not copied_count:
         new_name = _("Copy of %s") % (self.name)
-        else:
+    else:
         new_name = _("Copy of %s (%s)") % (self.name, copied_count)
-        default['name'] = new_name
-        return super(Course, self).copy(default)
+    default['name'] = new_name
+    return super(Course, self).copy(default)
 
 
 class Session(models.Model):
