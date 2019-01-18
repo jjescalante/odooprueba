5201e0d3200e4c404efc4905f09470ec081ac945
Jose Juan Escalante <jjet@live.com.mx>
2019-01-18 13:08:25 -0600

    [REF] new .travis.yml

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': "openacademy",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
 
     'author': "My Company",
     'website': "http://www.yourcompany.com",
