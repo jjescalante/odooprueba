1a14d547f5f321a826bb46ca728664169a3bebb9
Jose Juan Escalante <jjet@live.com.mx>
2019-01-21 21:45:17 -0600

    [REF] test

--- openacademy/tests/test_global_openacademy_course.py
+++ openacademy/tests/test_global_openacademy_course.py
@@ -1,38 +1,3 @@
 # -*- encoding: utf-8 -*-
 
 from openerp.tests.common import TransactionCase
-
-
-#class GlobalTestOpenAcademyCourse(TransactionCase):
-#   '''
-
-#    Global test to openacademy course model.
-#   Test create course and trigger contraints.
-
-#    '''
-
-    # Method seudo-constructor of test setUp
-    #def setUp(self):
-        # Define global variables to test methods
-    #    super(GlobalTestOpenAcademyCourse, self).setUp()
-    #    self.variable = "hello world"
-    #   self.env['openacademy_course']
-
-    # Method of class that don't is test
-    #def create_course(self, course_name,
-    #                  course_description,
-    #                  course_responsible_id):
-    #    course_id = self.course.create({
-    #        'name': course_name,
-    #        'description': course_description,
-    #        'responsible': course_responsible_id,
-    #    })
-        return course_id
-
-    # Method of test startswith 'def test_*(self):'
-    #def test_01_same_name_description(self):
-    #    '''
-    #    Test create a course with same name and descriptrion
-    #    to test contraint of name different to description.
-    #    '''
-    #   self.create_course('test1', 'test', None)
